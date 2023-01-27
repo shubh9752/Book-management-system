357b3a844291502f72fba83052dd1455e0be985e
shubh9752 <61161071+shubh9752@users.noreply.github.com>
2023-01-27 23:32:41 +0530

    user routes created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const app=express();
 const PORT=8081;
+//importing json data from data folder
+const {books}=require("./data/books.json");
+const {users}=require("./data/users.json");
 
 app.use(express.json());
 
@@ -9,6 +12,76 @@ app.get("/",(req,res)=>{
         message:"congrats! server is running successfuly"
     })
 });
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        data:users,
+    });
+});
+app.get("/users/:id",(req,res)=>{
+    const {id}=req.params
+    const user=users.find((each)=>each.id===id);
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"user not found"
+        })
+    }
+    return res.status(200).json({
+        success:true,
+        data:user,
+    })
+});
+app.put("/users/:id",(req,res)=>{
+    const{id}=req.params;
+    const{data}=req.body;
+
+    const user=users.find((each)=>each.id===id);
+
+    if(!user){
+        return res.status(404).json({success:false,
+        message:"user not exist",
+       });
+    }
+    const updatingUser=users.map((each)=>{
+        if(each.id===id){
+            return {
+                ...each,
+                ...data,
+            }
+        }
+        return each;
+    })
+    return res.status(200).json({
+        success:true,
+        data:updatingUser,
+    })
+});
+app.post("/users",(req,res)=>{
+    const {id,name,surname,email,subscriptionType,subscriptionDate}=req.body;
+    const user=users.find((each)=>each.id===id);
+
+    if(user){
+        return res.status(404).json({
+            success:false,
+            message:"user allready exist",
+        });
+
+    }
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+    return res.status(202).json({
+        success:true,
+        data:users,
+    })
+})
 app.get("*",(req,res)=>{
     res.status(404).json({
         message:"this rout is not exist"
